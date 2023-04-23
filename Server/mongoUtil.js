e3fbf64145842dbf737e06d19c58a97ea79c62e5
Alexander <swedcoder@gmail.com>
2023-04-23 19:48:53 +0200

    Changed env name

--- Server/mongoUtil.js
+++ Server/mongoUtil.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-const uri = process.env.MONGODB_CONNECTION;
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 let db;
 
